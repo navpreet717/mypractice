a74db9538a2b7dfd8a6ba56efc508fb5b563a75f
BAJWA <BAJWA@192.168.0.10>
2019-06-16 12:55:07 -1000

    string ""

---
+++ Strr.java
@@ -0,0 +1,23 @@
+package practice;
+
+public class Strr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+String p = "Many years ago, people used horses instead of cars";
+String m = p.substring(10,15);
+
+	System.out.println(m);
+	String pp = "C#...is tricky";
+	int n = pp.length();
+	System.out.println(n);
+	String j = "David";
+	System.out.println("\""+j+"\"");
+	
+	String k="jkj";
+	System.out.println(j+"\n"+k);
+	
+	}
+
+}
