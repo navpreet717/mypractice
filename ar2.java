64d9fb17b4109bbca71fd9a11337ecc285a55b10
BAJWA <BAJWA@192.168.0.10>
2019-06-17 21:38:13 -1000

    update

---
+++ ar2.java
@@ -0,0 +1,25 @@
+package practice;
+
+public class ar2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1
+		int[] sgt=new int[800];
+		//2
+		double[] dfw = new double[21];
+		System.out.println(dfw.length);
+		//3
+		char[] cr=new char[]{'a','b','c','d','e'};
+		System.out.println(cr);
+		//squareroot
+sgt[0]=100;
+sgt[1]=2;
+for (int i = 0; i < sgt.length; i++) {
+  int square = sgt[i] * sgt[i];
+  System.out.println(square);
+  //negative number
+ 
+
+
+}}}
